299be8157fd7d427ad43f4bbcd30ab9e95193daf
hungdt <damtienhung_t62@hus.edu.vn>
2020-05-01 20:25:40 +0700

    Update migration

--- database/migrations/2020_05_01_125851_create_table_subjects.php
+++ database/migrations/2020_05_01_125851_create_table_subjects.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTableSubjects extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('subjects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->integer('role');
-            $table->string('code');
+            $table->string('name', 30);
+            $table->string('code', 10)->unique();
+            $table->date('started_at');
+            $table->integer('number_lessons_per_week');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('subjects');
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      DB:table('users')->insert([
+        'email' => 'hungdt@gmail.com',
+        'name' => 'Dam Tien Hung',
+        'role' => 2,
+        'code' => '17001201',
+        'password' => Hash::make('12345678'),
+
+      ]);
       DB::table('students')->insert([
         'name' => 'Dam Tien Hung',
         'email' => 'hungdt@gmail.com',
-        'code' => '1',
+        'code' => '17001201',
       ]);
       $array = array();
       $array["MAT3021"] = "Giải tích 1";
